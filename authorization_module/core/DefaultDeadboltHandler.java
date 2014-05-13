e0890168d957c3706cd05ef6d36b49ab44739f7a
Mateusz Jaje <mateusz.jaje@sigmapoint.co>
2014-05-13 11:46:20 +0200

    added more verbose output from auth failed

--- authorization_module/core/DefaultDeadboltHandler.java
+++ authorization_module/core/DefaultDeadboltHandler.java
@@ -4,9 +4,13 @@ import authorization_module.authorization_module.AuthorizationFacade;
 import be.objectify.deadbolt.core.models.Subject;
 import be.objectify.deadbolt.java.DeadboltHandler;
 import be.objectify.deadbolt.java.DynamicResourceHandler;
+import play.Logger;
 import play.mvc.Controller;
+import play.mvc.Http;
 import play.mvc.Http.Context;
 import play.mvc.Result;
+
+import static java.lang.String.format;
 
 public class DefaultDeadboltHandler implements DeadboltHandler {
 
@@ -36,7 +40,16 @@ public class DefaultDeadboltHandler implements DeadboltHandler {
 
 	@Override
 	public Result onAuthFailure(Context arg0, String context) {
-		return Controller.forbidden();
+
+		String sessionToken = SessionTokenHelper.getSessionTokenFromHeader(arg0);
+		if (sessionToken == null) {
+			return Controller.forbidden(format("no session token"));
+		}
+		UserSession userSessionFromDB = UserSession.find.where().eq("token", sessionToken).findUnique();
+		if (userSessionFromDB == null) {
+			return Controller.forbidden(format("no session for token %s", sessionToken));
+		}
+		return Controller.forbidden("access forbidden");
 	}
 
 }
